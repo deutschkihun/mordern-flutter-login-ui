3388fcc4674d2d1b7750047c6c2a4582d4450335
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2023-03-04 11:15:36 +0900

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mordern_login_ui/pages/login_page.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
